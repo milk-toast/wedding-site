22668e277dc5860c5106940453cd84255b01c864
milk-toast <53436675+milk-toast@users.noreply.github.com>
2020-05-25 12:05:38 -0400

    fixed mobile bugs

---
+++ assets/js/nav.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+			$(".myNavbarTogglerOpen").click(function(){
+				$(".myNavbarTogglerOpen").hide(200);
+				$(".myNavbarTogglerClose").show(200);
+				$(".navbarOverlay").show(200);
+			});
+			$(".myNavbarTogglerClose").click(function(){
+				$(".myNavbarTogglerClose").hide(200);
+				$(".myNavbarTogglerOpen").show(200);
+				$(".navbarOverlay").hide(200);
+			});
+		});
